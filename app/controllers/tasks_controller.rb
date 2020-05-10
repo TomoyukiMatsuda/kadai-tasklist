8d13159a5ce353fc646238965d490c79d4b189b8
matsuda.tomoyuki <m.u.p.s.4.15.17@gmail.com>
2020-05-10 09:10:07 +0000

    fix correct_user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
   
   def index
@@ -55,14 +55,16 @@ class TasksController < ApplicationController
     params.require(:task).permit(:content, :status)
   end
   
-  def set_task
-    @task = Task.find(params[:id])
-  end
-  
   def require_user_logged_in
     unless logged_in?
       redirect_to login_url
     end
   end
-
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 end
